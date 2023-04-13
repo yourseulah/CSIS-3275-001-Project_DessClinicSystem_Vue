c985cf4fd3051c0589973c1b22a65074c810b995
David Salazar <dssalaza@gmail.com>
2023-04-13 11:16:28 -0700

    Formatted index

--- dessclinicsystem/src/router/index.js
+++ dessclinicsystem/src/router/index.js
@@ -1,7 +1,7 @@
 import { createWebHistory, createRouter } from "vue-router";
 import HomePage from "../components/HomePage.vue";
 import PatientList from "../components/Patient/PatientList.vue";
-import AppointmentInfo from "@/components/Appointment/AppointmentInfo"
+import AppointmentInfo from "@/components/Appointment/AppointmentInfo";
 import AppointmentList from "../components/Appointment/AppointmentList.vue";
 import AppointmentCreate from "../components/Appointment/AppointmentCreate.vue";
 import DoctorList from "../components/Doctor/DoctorList.vue";
@@ -16,86 +16,86 @@ import PatientDetails from "@/components/Patient/PatientDetails";
 import InvoiceDetails from "../components/Invoice/InvoiceDetails";
 
 const routes = [
-    {
-        name: "HomePage",
-        path: "/",
-        component: HomePage
-    },
-    {
-        name: "PatientList",
-        path: "/patientlist",
-        component: PatientList
-    },
-    {
-        name: "DoctorList",
-        path: "/doctorlist",
-        component: DoctorList
-    },
-    {
-        name: "AppointmentInfo",
-        path: "/appointmentinfo",
-        component: AppointmentInfo
-    },
-    {
-        name: "AppointmentList",
-        path: "/appointmentlist",
-        component: AppointmentList
-    },
-    {
-        name: "AppointmentCreate",
-        path: "/appointmentcreate",
-        component: AppointmentCreate
-    },
-    {
-        name: "InvoiceList",
-        path: "/invoicelist",
-        component: InvoiceList
-    },
-    {
-        name: "PatientAdd",
-        path: "/patientadd",
-        component: PatientAdd
-    },
-    {
-        name: "PatientInfo",
-        path: "/patientinfo",
-        component: PatientInfo
-    },
-    {
-        name: "PatientUpdate",
-        path: "/patients/:id",
-        component: PatientUpdate
-    },
-    {
-        name: "PatientUpdate2",
-        path: "/patients2/:id",
-        component: PatientUpdate2
-    },
-    {
-        name: "PatientSearch",
-        path: "/patientSearch",
-        component: PatientSearch
-    },
-    {
-        name: "PatientSearch2",
-        path: "/patientSearch2",
-        component: PatientSearch2
-    },
-    {
-        name: "PatientDetails",
-        path: "/patient/:id",
-        component: PatientDetails
-    },
-    {
-        name: "InvoiceDetails",
-        path: "/invoice/:id",
-        component: InvoiceDetails
-    }
+  {
+    name: "HomePage",
+    path: "/",
+    component: HomePage,
+  },
+  {
+    name: "PatientList",
+    path: "/patientlist",
+    component: PatientList,
+  },
+  {
+    name: "DoctorList",
+    path: "/doctorlist",
+    component: DoctorList,
+  },
+  {
+    name: "AppointmentInfo",
+    path: "/appointmentinfo",
+    component: AppointmentInfo,
+  },
+  {
+    name: "AppointmentList",
+    path: "/appointmentlist",
+    component: AppointmentList,
+  },
+  {
+    name: "AppointmentCreate",
+    path: "/appointmentcreate",
+    component: AppointmentCreate,
+  },
+  {
+    name: "InvoiceList",
+    path: "/invoicelist",
+    component: InvoiceList,
+  },
+  {
+    name: "PatientAdd",
+    path: "/patientadd",
+    component: PatientAdd,
+  },
+  {
+    name: "PatientInfo",
+    path: "/patientinfo",
+    component: PatientInfo,
+  },
+  {
+    name: "PatientUpdate",
+    path: "/patients/:id",
+    component: PatientUpdate,
+  },
+  {
+    name: "PatientUpdate2",
+    path: "/patients2/:id",
+    component: PatientUpdate2,
+  },
+  {
+    name: "PatientSearch",
+    path: "/patientSearch",
+    component: PatientSearch,
+  },
+  {
+    name: "PatientSearch2",
+    path: "/patientSearch2",
+    component: PatientSearch2,
+  },
+  {
+    name: "PatientDetails",
+    path: "/patient/:id",
+    component: PatientDetails,
+  },
+  {
+    name: "InvoiceDetails",
+    path: "/invoice/:id",
+    component: InvoiceDetails,
+  },
 ];
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes
-})
+  history: createWebHistory(),
+  routes,
+});
 
 export default router;
